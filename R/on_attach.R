961bc8aac5e3bb338f94b90093ac8428a8ee629a
davharris <harry491@gmail.com>
2015-05-11 17:31:32 -0700

    Note that Archived version is outdated

---
+++ R/on_attach.R
@@ -0,0 +1,5 @@
+.onAttach <- function(...) {
+  packageStartupMessage(
+    "This is the Ecological Archives version of `rosalia`, which may be outdated.\nDownload the most recent version from https://github.com/davharris/rosalia"
+  )
+}
