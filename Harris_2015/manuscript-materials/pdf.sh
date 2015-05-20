b14527e09dc1356deacff3be5f0ea0fe6f223334
davharris <harry491@gmail.com>
2015-05-19 22:15:53 -0700

    add manuscript materials

---
+++ Harris_2015/manuscript-materials/pdf.sh
@@ -0,0 +1,11 @@
+# bibtex ids with colons in them (eg R package names) don't work. Remove the pattern :_
+sed -i "" "s/:_/_/g" My\ Library.bib
+
+pandoc -o draft.tex --template=mytemplate.latex --bibliography=My\ Library.bib --csl=ecology.csl draft.Rmd
+
+pandoc -o draft.pdf --template=mytemplate.latex --bibliography=My\ Library.bib --csl=ecology.csl draft.Rmd
+
+pandoc -o Appendices.pdf --bibliography=My\ Library.bib --csl=ecology.csl Appendices.Rmd
+
+open draft.pdf
+open Appendices.pdf
