013e2c4d2498b57909d78b39a294241781cbdc57
davharris <harry491@gmail.com>
2015-07-15 10:35:54 -0700

    more

--- stochastic/dynamic_simulation.R
+++ stochastic/dynamic_simulation.R
@@ -1,16 +1,18 @@
+set.seed(1)
+
 n_spp = 15
-K = 20
+K = 15
 
 maxit = 1E7
-thin = 4E3
+thin = 2E4
 
 seed_rain = rep(.1, n_spp)
 growth = rep(1, n_spp)
 
 competition_vector = ifelse(
   rbinom(choose(n_spp, 2), size = 1, prob = .5),
-  rexp(choose(n_spp, 2), 5),
-  rexp(choose(n_spp, 2), 1/5)
+  rexp(choose(n_spp, 2), 2),
+  rexp(choose(n_spp, 2), 1/2)
 )
 
 competition = matrix(0, nrow = n_spp, ncol = n_spp)
@@ -50,35 +52,54 @@ for(i in 1:maxit){
 matplot(t(pops), type = "l", lty = 1)
 
 
-library(corpcor)
-pcor = pcor.shrink(t(pops))
-class(pcor) = "matrix"
-lattice::levelplot(pcor^2)
-
-plot(pcor[upper.tri(pcor)], -competition_vector)
-
 library(rosalia)
 fit = rosalia(
   t(pops > 0) * 1, 
   hessian = TRUE, 
-  maxit = 1E3
+  maxit = 1E3,
+  prior = make_logistic_prior(location = 0, scale = 2)
+)
+
+
+
+library(BayesComm)
+bc = BC(Y = t(pops > 0), model = "community", its = 1000, thin = 10)
+
+
+# Markov network performance
+cor(fit$beta[upper.tri(fit$beta)], -competition_vector, method = "spearman")
+
+# Correlation performance
+cor(
+  cor(t(pops > 0))[upper.tri(cor(t(pops > 0)))], 
+  -competition_vector, method = "spearman"
+)
+
+# Partial correlation performance
+cor( 
+  -solve(cor(t(pops > 0)))[upper.tri(cor(t(pops > 0)))], 
+  -competition_vector, 
+  method = "spearman"
+)
+
+# BayesComm performance
+cor( 
+  colMeans(bc$trace$R), 
+  -competition_vector, 
+  method = "spearman"
 )
 
 mles = fit$beta[upper.tri(fit$beta)]
 ses = sqrt(diag(solve(fit$opt$hessian)))[-(1:n_spp)]
 is_significant = ifelse(mles > 0, mles - 2 * ses > 0, mles + 2 * ses < 0)
 
-
-cor(fit$beta[upper.tri(fit$beta)], -competition_vector, method = "spearman")
 plot(
   fit$beta[upper.tri(fit$beta)], 
   -competition_vector,
   col = 1 + is_significant
 )
 abline(v = 0, h = 0)
-cor(
-  cor(t(pops > 0))[upper.tri(cor(t(pops > 0)))], 
-  -competition_vector, method = "spearman"
-)
 
-
+#xy = matrix(numeric(0), nrow = 0, ncol = 2)
+xy = rbind(xy, cbind(fit$beta[upper.tri(fit$beta)], -competition_vector))
+plot(xy, col = 1 + (0:(nrow(xy)-1) %/% choose(n_spp, 2)))
